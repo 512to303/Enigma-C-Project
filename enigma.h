ef5b5089d429fa037732e22891a5caa1a6a839a6
Jack Danielski <jackdski555@gmail.com>
2017-04-01 22:32:56 -0700

    Update enigma.h
    Problems with the setRotors function

--- enigma.h
+++ enigma.h
@@ -10,6 +10,12 @@
 #include <stdlib.h>
 
 
+/* 
+*
+*	Function Prototypes
+*
+*/
+
 // Takes text from a file and uses it as the message being encrypted 
 // or decrypted. 
 void textEnter(); 
@@ -33,27 +39,37 @@ void menuTwo();
 // Display help menu
 void help();
 
+//Give the rotors values
+void setRotors(void);
 
 
+/*
+*
+*	Complete functions
+*
+*/
 void menuOne() {
-	printf("Choose to encrypt, decrypt, or see help menu:\n");
+	char option[2];
 
+	printf("Choose to encrypt, decrypt, or see help menu:\n");
 	printf("
 		Encryption: -e\n
 		Decryption: -d\n
-		Help: -h
+		Help: -h\n
+		Enter an option: 
 		");
 
-	char choice[2];
-	scanf("Choose an option: %s\n", choice);
+	scanf("%s", option);
+	// Prints the option chosen as a test
+	//printf("%s\n", option);
 
-	if (strcmp(choice,"-e")) {
+	if ( strcmp(choice,"-e") == 0 ) {
 		encrypt();
 	}
-	else if (strcmp(choice, "-d")) {
+	else if ( strcmp(choice, "-d") == 0) {
 		decrypt();
 	}
-	else if (strcmp(choice,"-h")) {
+	else if ( strcmp(choice,"-h") == 0) {
 		help();
 	}
 	else {
@@ -91,5 +107,54 @@ void help() {
 		on your computer.  There are three main parts to the Enigma Machine: \ 
 		the Rotors, the Ringstellung, and the Plugboard.\n\
 		");
+
+}
+
+void setRotors(void) {
+	int rotorOne, rotorTwo, rotorThree; 
+
+	printf("Enter character for Rotor #1: ");
+	sscanf("%i", rotorOne);
+	if ( !(rotorOne > 64 && rotorOne < 91) ) {
+		// If lowercase make uppercase
+		if ( rotorOne > 96 && rotorOne < 122 ) {
+			rotorOne = rotorOne - 32;
+		}
+		else {
+			printf("Characters in your input are not accepted.\
+				Look at the help menu for a list of characters\ 
+				that are not accepted.\n");
+		}
+	}
+
+	printf("Enter character for Rotor #2: ");
+	sscanf("%i", rotorTwo);
+	if ( !(rotorTwo > 64 && rotorTwo < 91) ) {
+		// If lowercase make uppercase
+		if ( rotorTwo > 96 && rotorTwo < 122 ) {
+			rotorTwo = rotorTwo - 32;
+		}
+		else {
+			printf("Characters in your input are not accepted.\
+				Look at the help menu for a list of characters\ 
+				that are not accepted.\n");
+		}
+	}
+
+	printf("Enter character for Rotor #3: ");
+	sscanf("%i", rotorThree);
+	if ( !(rotorThree > 64 && rotorThree < 91) ) {
+		// If lowercase make uppercase
+		if ( rotorThree > 96 && rotorThree < 122 ) {
+			rotorThree = rotorThree - 32;
+		}
+		else {
+			printf("Characters in your input are not accepted.\
+				Look at the help menu for a list of characters\ 
+				that are not accepted.\n");
+		}
+	}
+
+	printf("Rotors are: %i, %i, %i \n", rotorOne, rotorTwo, rotorThree);
 
 }
